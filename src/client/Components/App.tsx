ee604a02a5c31418e3ea7c6d7ff0df078caead48
Dhamar Haryadi <dhamar.haryadi@connect.qut.edu.au>
2019-10-25 16:00:31 +1000

    Add pie chart

--- src/client/Components/App.tsx
+++ src/client/Components/App.tsx
@@ -1,5 +1,8 @@
-import React, { useState, FC, CSSProperties } from 'react'
-import { LineChart, CartesianGrid, XAxis, YAxis, Line, Tooltip } from 'recharts'
+import React, { useState, FC, CSSProperties, useEffect } from 'react'
+import { LineChart, CartesianGrid, XAxis, YAxis, Line, Tooltip, PieChart, Pie, Cell } from 'recharts'
+// import {
+//   PieChart, Pie, Sector, Cell,
+// } from 'recharts';
 import useInterval from '@use-it/interval'
 
 const App: FC = () => {
@@ -9,6 +12,44 @@ const App: FC = () => {
   const [error, setError] = useState('')
   const [clicked, setClicked] = useState(false)
   const [lastUpdated, setLastUpdate] = useState<Date>(null)
+
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    let totalPositive = 0;
+    let totalNegative = 0;
+    let totalNeutral = 0;
+    server.forEach(tweet => {
+      if (tweet.sentiment > 0) totalPositive++;
+      if (tweet.sentiment < 0) totalNegative++;
+      if (tweet.sentiment === 0) totalNeutral++;
+    })
+    setData([
+      { name: 'Positive', value: totalPositive },
+      { name: 'Negative', value: totalNegative },
+      { name: 'Neutral', value: totalNeutral },
+    ])
+
+  },[server])
+
+  const COLORS = ['blue', 'red', 'yellow'];
+
+  /**
+   * renderCustomizedLabel. Does not work
+   */
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({
+    cx, cy, midAngle, innerRadius, outerRadius, percent, index,
+  }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+      <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  };
 
   const style: CSSProperties = {
     fontFamily: 'sans-serif',
@@ -26,6 +67,7 @@ const App: FC = () => {
             (e, i, arr) => i === arr.findIndex(t => t.tweetId === e.tweetId)
           )
         )
+
         setLastUpdate(new Date())
       } catch (error) {
         setError(error)
@@ -94,40 +136,64 @@ const App: FC = () => {
       {loading ? (
         <h1 style={style}>Loading</h1>
       ) : (
-        server.length > 0 && (
-          <LineChart
-            width={1000}
-            height={500}
-            margin={{ left: 20, bottom: 20 }}
-            style={{
-              margin: 'auto'
-            }}
-            data={server
-              .filter(e => e.sentiment !== 0)
-              .map(e => ({
-                name: e.userScreenName,
-                sentiment: e.sentiment
-              }))}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis
-              dataKey="name"
-              label={{ value: 'Users', position: 'insideBottom', offset: -10 }}
-            />
-
-            <YAxis
-              label={{
-                value: 'Sentiment',
-                angle: -90,
-                position: 'insideLeft',
-                offset: -10
+          server.length > 0 && (
+            <LineChart
+              width={1000}
+              height={500}
+              margin={{ left: 20, bottom: 20 }}
+              style={{
+                margin: 'auto'
               }}
-            />
-            <Tooltip />
-            <Line dataKey="sentiment" stroke="#8884d8" />
-          </LineChart>
-        )
-      )}
+              data={server
+                .filter(e => e.sentiment !== 0)
+                .map(e => ({
+                  name: e.userScreenName,
+                  sentiment: e.sentiment
+                }))}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis
+                dataKey="name"
+                label={{ value: 'Users', position: 'insideBottom', offset: -10 }}
+              />
+
+              <YAxis
+                label={{
+                  value: 'Sentiment',
+                  angle: -90,
+                  position: 'insideLeft',
+                  offset: -10
+                }}
+              />
+              <Tooltip />
+              <Line dataKey="sentiment" stroke="#8884d8" />
+            </LineChart>
+          )
+        )}
+
+
+
+      {loading ? (
+        <h1 style={style}>Loading</h1>
+      ) : (
+          server.length > 0 && (
+            <PieChart width={800} height={800}>
+              <Pie
+                data={data}
+                labelLine={false}
+                // label={renderCustomizedLabel} does not work
+                fill="#8884d8"
+                dataKey="value"
+              >
+
+                {
+                  data.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
+                }
+              </Pie>
+            </PieChart>
+          )
+        )}
+
       {error.length > 0 && <h1>Error Fetching new data {error}</h1>}
     </div>
   )
