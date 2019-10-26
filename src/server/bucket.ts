2f69c24a1291dbaa41a2aea10913954cbdf43e5d
Quintisimo <quintuscardozo13@gmail.com>
2019-10-26 19:40:57 +1000

    removed unused import

--- src/server/bucket.ts
+++ src/server/bucket.ts
@@ -1,6 +1,6 @@
-import { config, S3 } from 'aws-sdk'
+import { S3 } from 'aws-sdk'
 
-const Bucket = `tweetmotion-store`
+const Bucket = `tweetmotion`
 const apiVersion = '2006-03-01'
 const s3 = new S3({ apiVersion })
 
